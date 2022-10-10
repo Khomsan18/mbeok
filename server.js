e6e389241da16d44f4873ac0aa03e8bfb14f85ed
Khomsan18 <81680948+Khomsan18@users.noreply.github.com>
2022-10-10 14:52:49 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -83,9 +83,34 @@ app.delete('/user/:user_id', function(req,res){
        
     });    
 })
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-
+app.get("/allebook", function (req, res) {
+    dbConn.query("SELECT * FROM ebooks", function (error, results, fields) {
+      if (error) throw error;
+      return res.send(results);
+    });
+  });
+  
+  app.post("/ebk", function (req, res) {
+    var ebook = req.body;
+  
+    if (!ebook) {
+      return res
+        .status(400)
+        .send({ error: true, message: "Please provide ebooks " });
+    }
+  
+    dbConn.query(
+      "INSERT INTO ebooks SET ? ",
+      ebook,
+      function (error, results, fields) {
+        if (error) throw error;
+        return res.send(results);
+      }
+    );
+  });
+  
 //set port
 app.listen(3000, function () {
     console.log('Node app is running on port 3000');
